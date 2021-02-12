6de6e7647441293a4588c4f816bd5aff9432fc17
John Demian <john.demian@procontent.net>
2021-02-12 15:20:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import Vue from "vue";
+import * as Sentry from "@sentry/vue";
 
+Sentry.init({
+  Vue: Vue,
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+});
 new Vue({
   render: h => h(App),
 }).$mount('#app')
